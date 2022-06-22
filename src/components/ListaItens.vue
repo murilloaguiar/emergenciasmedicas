9fc8cb9b53843bc69bc4a701bb99f290ba9f8c71
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-06-22 09:39:08 -0300

    Visualizar a relação de profissionais

--- src/components/ListaItens.vue
+++ src/components/ListaItens.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
-        Lista Itens {{ tipo }}
-        <br>
-        <item />
+        <item 
+            v-for="(item, indice) in itens" 
+            :key="indice" 
+            :dados="item"
+        />
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import Item from '@/components/Item.vue'
 
 export default {
@@ -16,6 +19,33 @@ export default {
     },
     props: {
         tipo: String
+    },
+
+    computed:{
+        ...mapState({
+            enfermeiros: state=> state.enfermeiros,
+            socorristas: state=> state.socorristas,
+            medicos: state=> state.medicos,
+        }),
+
+        itens(){
+            switch (this.tipo) {
+                //dentro propriedades computadas podemos acessar outras propriedades computadas, basta adicionar o operador this.
+                case 'enfermeiros':
+                    return this.enfermeiros
+                    break;
+                case 'socorristas':
+                    return this.socorristas
+                    break;
+                case 'medicos':
+                    return this.medicos
+                    break;
+            
+                default:
+                    return []
+                    break;
+            }
+        }
     }
 }
 </script>
