8f39478488877263a0b68efd2187dc4cfe09983c
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-06-22 16:49:10 -0300

    mapGetters, reutilizar e encaminhar parâmetros

--- src/components/ListaItens.vue
+++ src/components/ListaItens.vue
@@ -1,65 +1,81 @@
 <template>
-    <div>
-        <item 
-            v-for="(item, indice) in itens" 
-            :key="indice" 
-            :dados="item"
-        />
-    </div>
+   <div>
+      <div v-if="tipo == 'socorristas'">
+         <select class="form-control form-control-sm" v-model="turno">
+            <option value="">Todos</option>
+            <option value="manhã">Manhã</option>
+            <option value="tarde">Tarde</option>
+            <option value="noite">Noite</option>
+         </select>
+      </div>
+      <item v-for="(item, indice) in itens" :key="indice" :dados="item" />
+
+      <div v-if="tipo == 'socorristas'">
+         Total {{totalSocorristasPorTurno(turno)}}
+      </div>
+   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import Item from '@/components/Item.vue'
+import { mapState } from "vuex";
+import { mapGetters } from "vuex";
+import Item from "@/components/Item.vue";
 
 export default {
-    name: 'ListaItens',
-    components: { 
-        Item
-    },
-    props: {
-        tipo: String
-    },
+   name: "ListaItens",
+   components: {
+      Item,
+   },
 
-    computed:{
-        ...mapState({
-            enfermeiros: state=> state.enfermeiros,
-            //socorristas: state=> state.socorristas,
-            medicos: state=> state.medicos,
-            carros: state => state.equipamentos.carros,
-            telefones: state => state.equipamentos.telefones,
-            kitsDeReanimacao: state => state.equipamentos.kitsDeReanimacao
-        }),
+   data: () => ({
+      turno: "",
+   }),
 
-        itens(){
-            switch (this.tipo) {
-                //dentro propriedades computadas podemos acessar outras propriedades computadas, basta adicionar o operador this.
-                case 'enfermeiros':
-                    return this.enfermeiros
-                    
-                case 'socorristas':
-                    return this.$store.getters.socorristasPorTurno
-                    
-                case 'medicos':
-                    return this.medicos
-                    
-                    
-                case 'carros':
-                    return this.carros
-                    
-                    
-                case 'telefones':
-                    return this.telefones
-                    
-                    
-                case 'kits-de-reanimacao':
-                    return this.kitsDeReanimacao
-                    
-                default:
-                    return []
-                    
-            }
-        }
-    }
-}
+   props: {
+      tipo: String,
+   },
+
+   computed: {
+      //spread
+      ...mapState({
+         enfermeiros: (state) => state.enfermeiros,
+         //socorristas: state=> state.socorristas,
+         medicos: (state) => state.medicos,
+         carros: (state) => state.equipamentos.carros,
+         telefones: (state) => state.equipamentos.telefones,
+         kitsDeReanimacao: (state) => state.equipamentos.kitsDeReanimacao,
+      }),
+
+      //...mapGetters(['socorristasPorTurno','totalSocorristasPorTurno']), array
+      ...mapGetters({
+         totalSocorristasPorTurno: 'totalSocorristasPorTurno',
+         socorristasPorTurno: 'socorristasPorTurno'
+      }),
+
+      itens() {
+         switch (this.tipo) {
+            case "enfermeiros":
+               return this.enfermeiros;
+
+            case "socorristas":
+               return this.socorristasPorTurno(this.turno);
+
+            case "medicos":
+               return this.medicos;
+
+            case "carros":
+               return this.carros;
+
+            case "telefones":
+               return this.telefones;
+
+            case "kits-de-reanimacao":
+               return this.kitsDeReanimacao;
+
+            default:
+               return [];
+         }
+      },
+   },
+};
 </script>
