1bcda226007813ea8193e1d96111fc86f5a181f2
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-06-22 08:19:55 -0300

    Implementar mapState() notação de array

--- src/store.js
+++ src/store.js
@@ -4,6 +4,15 @@ import Vuex from 'vuex'
 
 export default new Vuex.Store({
    state:{
-      titulo: 'Emergências Médicas'
+      titulo: 'Emergências Médicas',
+      equipe:{
+         enfermeiro: 'Nome do enfermeiro',
+         socorrista:'Nome do socorrista',
+         medico: 'Nome do médico',
+         carro: 'Placa do carro',
+         telefone: 'Telefone qualquer',
+         kitDeReanimacao: 'Kit 0001'
+
+      }
    }
 })
