9d13dfc6ab9fe1b03b38380194ba598bcb4c5ed4
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-06-24 11:54:15 -0300

    Recuperar dados da api fake por meio do fetch

--- src/store.js
+++ src/store.js
@@ -2,93 +2,88 @@
 import Vuex from "vuex";
 
 export default new Vuex.Store({
-   state: {
-      titulo: "Emergências Médicas",
+  state: {
+    titulo: "Emergências Médicas",
 
-      equipe: {
-         enfermeiro: "",
-         socorrista: "",
-         medico: "",
-         carro: "",
-         telefone: "",
-         kitDeReanimacao: "",
-      },
+    equipe: {
+      enfermeiro: "",
+      socorrista: "",
+      medico: "",
+      carro: "",
+      telefone: "",
+      kitDeReanimacao: "",
+    },
 
-      enfermeiros: [
-         { id: 1, nome: "João", escala: "12x36" },
-         { id: 2, nome: "Maria", escala: "12x36" },
-         { id: 3, nome: "Ana", escala: "24x48" },
-         { id: 4, nome: "José", escala: "24x48" },
-      ],
-      socorristas: [
-         { id: 1, nome: "Marcos", turno: "manhã" },
-         { id: 2, nome: "Felipe", turno: "tarde" },
-         { id: 3, nome: "Cláudia", turno: "tarde" },
-         { id: 4, nome: "Michele", turno: "noite" },
-      ],
-      medicos: [
-         { id: 1, nome: "André", escala: "12x36" },
-         { id: 2, nome: "Roberta", escala: "12x36" },
-         { id: 3, nome: "Carlos", escala: "24x48" },
-         { id: 4, nome: "Juliana", escala: "24x48" },
-      ],
-      equipamentos: {
-         carros: [
-            { id: 1, placa: "ABC-0000" },
-            { id: 2, placa: "BRA1A11" },
-            { id: 3, placa: "CBA-1111" },
-            { id: 4, placa: "ARB2B22" },
-         ],
-         telefones: [
-            { id: 1, telefone: "+55 11 98888-8888" },
-            { id: 2, telefone: "+55 11 97777-7777" },
-            { id: 3, telefone: "+55 11 96666-6666" },
-            { id: 4, telefone: "+55 11 95555-5555" },
-         ],
-         kitsDeReanimacao: [
-            { id: 1, kit: "K0001" },
-            { id: 2, kit: "K0002" },
-            { id: 3, kit: "K0003" },
-            { id: 4, kit: "K0004" },
-         ],
-      },
-   },
+    enfermeiros: [],
+    socorristas: [],
+    medicos: [],
 
-   getters: {
-      totalEnfermeiros(state) {
-         return state.enfermeiros.length;
-      },
+    equipamentos: {
+      carros: [],
+      telefones: [],
+      kitsDeReanimacao: [],
+    },
+  },
 
-      socorristasPorTurno(state) {
-         //closure
-         return (turno) =>
-            !turno
-               ? state.socorristas
-               : state.socorristas.filter(
-                    (socorrista) => socorrista.turno === turno
-                 )
-      },
+  getters: {
+    totalEnfermeiros(state) {
+      return state.enfermeiros.length;
+    },
 
-      totalSocorristas: (state) => state.socorristas.length,
+    socorristasPorTurno(state) {
+      //closure
+      return (turno) =>
+        !turno
+          ? state.socorristas
+          : state.socorristas.filter(
+              (socorrista) => socorrista.turno === turno
+            );
+    },
 
-      totalSocorristasPorTurno: (state, getters) => turno => getters.socorristasPorTurno(turno).length, //closure
+    totalSocorristas: (state) => state.socorristas.length,
 
-   },
+    totalSocorristasPorTurno: (state, getters) => (turno) =>
+      getters.socorristasPorTurno(turno).length, //closure
+  },
 
-   mutations:{
-      setItemEquipe: (state, item) => {
-         // console.log(state)
-         // console.log(item)
+  mutations: {
+    setItemEquipe: (state, item) => {
+      // console.log(state)
+      // console.log(item)
 
-         let t = item.tipo
-         let d = item.dados
+      let t = item.tipo;
+      let d = item.dados;
 
-         t == 'enfermeiros' ? state.equipe.enfermeiro = d.nome : null
-         t == 'socorristas' ? state.equipe.socorrista = d.nome : null
-         t == 'medicos' ? state.equipe.medico = d.nome : null
-         t == 'carros' ? state.equipe.carro = d.placa : null
-         t == 'telefones' ? state.equipe.telefone = d.telefone : null
-         t == 'kits-de-reanimacao' ? state.equipe.kitDeReanimacao = d.kit : null
-      }
-   }
+      t == "enfermeiros" ? (state.equipe.enfermeiro = d.nome) : null;
+      t == "socorristas" ? (state.equipe.socorrista = d.nome) : null;
+      t == "medicos" ? (state.equipe.medico = d.nome) : null;
+      t == "carros" ? (state.equipe.carro = d.placa) : null;
+      t == "telefones" ? (state.equipe.telefone = d.telefone) : null;
+      t == "kits-de-reanimacao" ? (state.equipe.kitDeReanimacao = d.kit) : null;
+    },
+
+    setEnfermeiros: (state, payload) => {
+      state.enfermeiros = payload;
+    },
+
+    setSocorristas: (state, payload) => {
+      state.socorristas = payload;
+    },
+
+    setMedicos: (state, payload) => {
+      state.medicos = payload;
+    },
+
+    setCarros: (state, payload) => {
+      state.equipamentos.carros = payload;
+    },
+
+    setTelefones: (state, payload) => {
+      state.equipamentos.telefones = payload.telefones;
+    },
+
+    setKitsDeReanimacao: (state, { kitsDeReanimacao }) => {
+      state.equipamentos.kitsDeReanimacao = kitsDeReanimacao;
+    },
+  },
 });
